c1ee7f1b2ff104f81360f4bc99415f7e0b82cf6e
JoongSeokD <ljseokd@gmail.com>
2020-06-04 14:06:12 +0900

    공통 인터페이스 적용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +30,33 @@ class MemberRepositoryTest {
         assertEquals(member.getId(), findMember.getId());
         assertEquals(member.getUsername(), findMember.getUsername());
         assertEquals(member, findMember);
+    }
+
+    @Test
+    void basicCRUD() throws Exception {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        List<Member> all = memberRepository.findAll();
+
+        Long count = memberRepository.count();
+
+        //then
+        assertEquals(findMember1, member1);
+        assertEquals(findMember2, member2);
+        assertEquals(all.size(), 2);
+        assertEquals(count, 2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        Long count2 = memberRepository.count();
+        assertEquals(count2,0);
     }
 
 }
